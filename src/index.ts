92227c199f98ada97d837abbfefef300640cbe8d
Ben Moses <benmoses@webfront.co.uk>
2025-05-19 21:59:33 +0000

    feat(commander): added Commander and created default entry point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 #! /usr/bin/env node
 
-console.log("Hello")
+import { Command } from "commander";
+
+// Declare program
+const program = new Command();
+
+// Add actions to the CLI
+program
+  .action(() => {
+    console.log("Loaded commander");
+  })
+  .description("Scaffolds projects with @benjymoses preferred defaults");
+
+//Execute the CLI with the provided arguments
+program.parse(process.argv);
