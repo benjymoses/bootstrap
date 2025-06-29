666ea27b08f3c1e7936dcf10157e522365df051f
Ben Moses <benmoses@webfront.co.uk>
2025-06-29 20:21:45 +0100

    ✨ feat: added Biome generator (re #38)
    re #38

---
+++ src/typescript/custom/addBiome.ts
@@ -0,0 +1,54 @@
+import type { NodePlopAPI } from "plop";
+import type {
+	BootstrapAction,
+	BootstrapActionList,
+	BootstrapActionSetup,
+} from "../types/bootstrapAction.js";
+import type { BootstrapAnswers } from "../types/bootstrapAnswers.js";
+
+import { executeShellCommand } from "../utils/executeShellCommand.js";
+
+const ACTION_NAME = "add-biome";
+
+const addBiomeActionSetup: BootstrapActionSetup = (plop: NodePlopAPI) => {
+	return plop.setActionType(ACTION_NAME, () => {
+		executeShellCommand(`pnpm pkg set devDependencies.@biomejs/biome="^2.0.6"`);
+		executeShellCommand(
+			`pnpm pkg set scripts.lint="biome check --write --unsafe --no-errors-on-unmatched"`,
+		);
+		executeShellCommand(
+			`pnpm pkg set scripts.lint:lint="biome lint --write --unsafe --no-errors-on-unmatched"`,
+		);
+		executeShellCommand(
+			`pnpm pkg set scripts.lint:format="biome format --write --unsafe --no-errors-on-unmatched"`,
+		);
+
+		return "Bringing in Biome...";
+	});
+};
+
+const addBiomeActionList: BootstrapActionList = (
+	path?: string,
+	answers?: BootstrapAnswers,
+) => {
+	if (!path || !answers) {
+		return [];
+	}
+
+	return [
+		{
+			type: ACTION_NAME,
+		},
+		{
+			path: `${path}/biome.jsonc`,
+			templateFile: `./templates/ts/linting/biome.jsonc.hbs`,
+			type: "add",
+		},
+	];
+};
+
+export const addBiome: BootstrapAction = {
+	actionSetup: addBiomeActionSetup,
+	actionsList: addBiomeActionList,
+	name: ACTION_NAME,
+};
